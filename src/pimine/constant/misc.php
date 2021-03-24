368c61dbcc5e6ac7c6579cb9f48cb7b591d056a1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-24 23:51:35 +0200

    Create misc.php

---
+++ src/pimine/constant/misc.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace pimine\constant;
+
+class misc {
+	public const PIMINE_LOGO = "
+  	____  _ __  __ _            
+   |  _ \(_)  \/  (_)_ __   ___ 
+   | |_) | | |\/| | | '_ \ / _ \
+   |  __/| | |  | | | | | |  __/
+   |_|   |_|_|  |_|_|_| |_|\___|
+  ";
+}
