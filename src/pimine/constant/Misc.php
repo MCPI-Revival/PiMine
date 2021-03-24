1e670fa3d3b2a372a5beb67d29328e525e2d582c
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-24 23:54:28 +0200

    Update and rename misc.php to Misc.php

--- src/pimine/constant/Misc.php
+++ src/pimine/constant/Misc.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace pimine\constant;
 
-class misc {
+class Misc {
 	public const PIMINE_LOGO = "    ____  _ __  __ _            
    |  _ \(_)  \/  (_)_ __   ___ 
    | |_) | | |\/| | | '_ \ / _ \
