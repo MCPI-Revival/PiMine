7f0de29cffe724a4a3f18e17160720d9bd3fb4ae
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-04-06 11:16:17 +0300

    Update PiMine.php

--- src/pimine/PiMine.php
+++ src/pimine/PiMine.php
@@ -4,25 +4,6 @@ declare(strict_types = 1);
 
 namespace pimine;
 
-function requirePathOnce(string $dir): void {
-	if (is_dir($dir)) {
-		if ($dh = opendir($dir)) {
-			while ($file = readdir($dh)) {
-				if ($file != '.' && $file != '..') {
-					if (is_dir($dir . $file)) {
-						requirePathOnce($dir . $file . "/");
-					} elseif (strtolower(substr($dir . $file, -4)) === ".php") {
-						require_once($dir . $file);
-					}
-				}
-			}
-		}
-		closedir($dh);
-	}
-}
-
-//requirePathOnce(dirname(__file__) . "/");
-
 spl_autoload_register(function ($class_name) {
 	include dirname(dirname(__file__)) . "/" . str_replace("\\", "/", $class_name) . ".php";
 });
